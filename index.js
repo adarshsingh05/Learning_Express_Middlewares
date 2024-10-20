18a2e9c90aea99c3e78bda1ee2825b7ebe3c2f66
adarsh singh <adarsh.22bce7232@vitapstudent.ac.in>
2024-10-21 02:36:38 +0530

    middle ware morgan and next() defining own middle ware

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+// getting the pages
+
+app.use(morgan('tiny'))
+
+
+app.get('/', (req,res)=>{
+    res.send("Homepage")
+})
+app.get('/dogs', (req,res)=>{
+    res.send("woof")
+})
+
+app.listen(3000, ()=>{
+    console.log("started on the 3000")
+})
